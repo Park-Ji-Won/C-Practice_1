39206d6c039d62795961f5109bbf7e790cb60807
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-07-28 21:06:04 +0900

    char 변수와 아스키 코드

---
+++ C-Practice/text1_ASCII_Code.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+	printf("--------------------------------------------------");
+	printf("|10진수 | ASCII |10진수 | ASC|| | 10진수 | ASCII |");
+	printf("|  0    |  NULL | 32    |       |        |       |");
+
+}

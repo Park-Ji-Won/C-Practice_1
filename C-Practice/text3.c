3d31f29590a166b80a9fafcbea372e29f6ad79c1
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-07-28 21:14:57 +0900

    변수형 최종

--- C-Practice/text3.c
+++ C-Practice/text3.c
@@ -9,14 +9,16 @@ int main() {
 	float fl;
 	double du;
 
-	printf("char 형 변수 입력: ");
+	printf("char 형 변수 입력: ");//문자를 보관하는 변수를 선언
 	scanf("%c", &ch);
+	/*1바이트를 차지하는 char 해당 변수인 ch에 한글 입력하면 오류 => 혀용된 메모리 이상으로 집어
+	넣어 발생하는 오류인 버퍼 오퍼플로우가 발생 한다(Buffer Overflow)*/
 
-	printf("short 형 변수 입력: ");
+	printf("short 형 변수 입력: ");//정수형 변수=int
 	scanf("%hd", &sh);
 	printf("int 형 변수 입력: ");
 	scanf("%d", &i);
-	printf("long 형 변수 입력: ");
+	printf("long 형 변수 입력: ");//정수형 변수=int
 	scanf("%ld", &lo);
 
 	printf("float 형 변수 입력 : ");

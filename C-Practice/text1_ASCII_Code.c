e0dcac18eee05e984c3d21cbae7a07edc2b73245
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-08-01 18:44:01 +0900

    아스키 코드 표 구현해보기_1

--- C-Practice/text1_ASCII_Code.c
+++ C-Practice/text1_ASCII_Code.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
 int main() {
-	printf("--------------------------------------------------");
-	printf("|10진수 | ASCII |10진수 | ASC|| | 10진수 | ASCII |");
-	printf("|  0    |  NULL | 32    |       |        |       |");
+	printf("-------------------------------------------------------------------\n");
+	printf("|10진수 | ASCII |10진수 | ASC|| | 10진수 | ASCII | 10진수 | ASCII |\n");
+	printf("|  0    | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  1    | SOH   |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  2    | STX   |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  3    | ETX   |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  4    | EOT   |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  5    | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  6    | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  7    | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  8    | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  9    | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  10   | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  11   | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  12   | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  13   | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  15   | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  16   | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  17   | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  18   | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  19   | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  20   | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  21   | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  22   | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  23   | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  24   | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  25   | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  26   | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  27   | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  28   | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  29   | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  30   | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("|  31   | NULL  |   32  |  SP   |   64   |   @   |	96    |   .   |\n");
+	printf("-------------------------------------------------------------------\n");
 
+	return 0;
 }

fe7e07eec7978aeb507239e598c7a628d2e1d809
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-09-14 22:41:04 +0900

    using namespace

---
+++ CPP_fast_reviewing/ch02_3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+namespace A {
+	void printAll() {
+		cout << "A의 printAll함수" << endl;
+	}
+}
+
+namespace B {
+	void printAll() {
+		cout << "B의 printAll함수" << endl;
+	}
+}
+
+namespace C {
+	void printAll() {
+		cout << "C의 PrintAll함수" << endl;
+	}
+}
+
+int main() {
+	A::printAll();
+	B::printAll();
+	C::printAll();
+}

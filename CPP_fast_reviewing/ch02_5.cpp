95ae9b606287d1a2fdcad31bd732c9f3135c2df9
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-09-14 22:58:35 +0900

    using char[] and cin and for

---
+++ CPP_fast_reviewing/ch02_5.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	char name[7];
+	for (int i = 0; i < 3; i++) {
+		cout << "이름 입력 : ";
+		cin >> name;
+		cout << (i + 1) << "번 이름 :" << name << endl;
+	}
+}

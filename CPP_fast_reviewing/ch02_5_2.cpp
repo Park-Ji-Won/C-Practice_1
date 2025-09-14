1dda5b01224ca94092de3e4540a22ad398e33188
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-09-14 23:06:23 +0900

    using cin.getline

---
+++ CPP_fast_reviewing/ch02_5_2.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	char name[100];
+	for (int i = 0; i < 3; i++) {
+		cout << "이름 입력 : ";
+		cin.getline(name, 100, '\n');
+		cout << (i + 1) << "번 이름 : " << name << endl;
+	}
+}

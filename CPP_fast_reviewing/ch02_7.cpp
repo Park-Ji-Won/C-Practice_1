8877e5b0d1f2ad9c3c6f3e7a52b590521cd4c7ba
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-09-14 23:16:20 +0900

    using cin.getline() and getchar()

---
+++ CPP_fast_reviewing/ch02_7.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	char name[10];
+	char address[100];
+	char quit;
+	while (true) {
+		cout << "이름 : ";
+		cin >> name;
+		while (getchar() != '\n');
+		cout << "주소 : ";
+		cin.getline(address, 100, '\n');
+		cout << "이름 : " << name << endl;
+		cout << "주소 : " << address<< endl;
+		cout << "종료(q/Q) : ";
+		cin >> quit;
+		if (quit == 'q' or quit == 'Q') break;
+		else cout << "계속 입력하세요";
+	}
+}

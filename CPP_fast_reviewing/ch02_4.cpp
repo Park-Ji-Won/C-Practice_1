54a03181451a7997f901edc2faf316a0eb111dfa
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-09-14 22:46:58 +0900

    using cin and math

---
+++ CPP_fast_reviewing/ch02_4.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int width;
+	cout << "너비를 입력하세요 >>";
+	cin >> width;
+
+	int height;
+	cout << "높이를 입력하세요 >>";
+	cin >> height;
+
+	int area = width * height;
+
+	cout << "면적은 " << area << "\n";
+}

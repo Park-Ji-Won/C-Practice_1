125447bcbc907e13950f1599e8f708db9296b973
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-09-23 02:10:35 +0900

    string class 이용한 문자열 생성 및 출력

---
+++ CPP_fast_reviewing/ch04_11.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main() {
+	string str;
+	string address("대전시 동구 동대전로 171");
+	string copyAddress(address);
+	char text[] = { 'L','o','v','e',' ','C','+','+','\0' };
+	string title(text);
+	string* p = new string("C++");
+	p->append(" Great!!");
+
+	cout << str << endl;
+	cout << address << endl;
+	cout << copyAddress << endl;
+	cout << title << endl;
+	cout << *p << endl;
+
+	delete p;
+}

a4def12f6468e780a1d3fcd8134b33ec96ceedc2
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-09-25 00:04:39 +0900

    string 다루기

---
+++ CPP_fast_reviewing/ch04_12a.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string name = "kitae";
+	string alias = "kito";
+	int res = name.compare(alias);
+
+	(res == 0) ? cout << "두 문자열이 같다" << endl : (res < 0) ? cout << name << "이 앞에 옴" << endl : \
+		cout << alias << "앞에 옴" << endl;
+
+	string a = "I love C++";
+	cout << "a: " << a << endl;
+	a.insert(2, "really");
+	cout << "a.insert(2, \"really\") : " << a << endl;
+	a.replace(2, 11, "study");
+	cout << "a.replace(2,11,\"study\") : " << a << endl;
+
+	cout << "a.length : " << a.length() << endl;
+	cout << "a.size : " << a.size() << endl;
+	cout << "a.capacity :	" << a.capacity() << endl;
+
+	a.erase(0, 7);
+	cout << "a.erase(0,7) : " << a << endl;
+	a.clear();
+	cout << "a.clear() : " << a << endl;
+
+	string b = "I love C++";
+	string c = b.substr(2, 4);
+	string d = b.substr(2);
+	cout << "b : " << b << endl << "b.substr(2,4) : " << c << endl;
+	cout << "b.substr(2) : " << d << endl;
+}

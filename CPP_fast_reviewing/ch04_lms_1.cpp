a5db83ff27fb5ccc0d6965a3bfb28861469a01b5
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-10-01 01:09:04 +0900

    포인터에 관한 LMS 연습

---
+++ CPP_fast_reviewing/ch04_lms_1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+class Circle {
+	int radius;
+public:
+	Circle();
+	~Circle(){}
+	void setRadius(int r) {
+		radius = r;
+	}
+	double getArea() {
+		return 3.14 * radius * radius;
+	}
+};
+
+Circle::Circle() {
+	radius = 1;
+}
+
+int main() {
+	int n;
+
+	cout << "생성하고자 하는 원의 개수: ";
+	cin >> n;
+
+	if (n < 0) {
+		cout << "프로그램 종료" << endl;
+		return 0;
+	}
+}

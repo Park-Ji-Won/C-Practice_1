80ca3f467f332b3c68972382ad370f9c80082a90
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-10-01 03:32:55 +0900

    참조변수 및 포인터 선언

---
+++ CPP_fast_reviewing/ch05_3a.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "i" << '\t' << "n" << '\t' << "refn"
+		<< '\t' << "p 주소		" << "p의 값" << '\t' << "n의 주소" << endl;
+	int i = 1;
+	int n = 2;
+	cout << i << '\t' << n << endl;
+
+	int& refn = n;
+	n = 4;
+	refn++;
+	cout << i << '\t' << n << '\t' << refn << endl;
+
+	refn = i;
+	refn++;
+	cout << i << '\t' << n << '\t' << refn << endl;
+
+	int* p = &refn;
+	cout << i << '\t' << n << '\t' << refn << '\t' << p << '\t' << *p << '\t' << &n << endl;
+
+	*p = 20;
+	cout << i << '\t' << n << '\t' << refn << '\t' << p << '\t' << *p << '\t' << &n << endl;
+}

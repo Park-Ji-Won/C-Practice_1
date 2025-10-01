0c20dcb67a903d34b4bc3c91ccbc9a44242c6cdd
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-10-01 23:56:05 +0900

    참조 리턴 실습

---
+++ CPP_fast_reviewing/ch05_8a.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	char c = 'a';
+	cout << "C: " << c << endl;
+
+	char* p = &c;
+	cout << "*p = &c 이후 c : " << c << ", *p : " << *p << " , p주소 / c주소 : " << static_cast<void*>(p) << endl;
+
+	char& s = c;
+	cout << "&s = c 이후 c : " << c << ", *p : " << *p << " , p주소 / c주소 : " << static_cast<void*>(p)
+		<< " , 참조 s : " << s << endl;
+
+	*p = 'b';
+	cout << "*p = 'b' 이후 c : " << c << ", *p : " << *p << " , p주소 / c주소 : " << static_cast<void*>(p)
+		<< " , 참조 s : " << s << endl;
+
+	s = 'c';
+	cout << "s = 'c' 이후 c : " << c << ", *p : " << *p << " , p주소 / c주소 : " << static_cast<void*>(p)
+		<< " , 참조 s : " << s << endl;
+}

d4ec993784217adc37bb161e8c52c574afdb976e
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-10-01 23:56:18 +0900

    참조 리턴 실습 ver2

---
+++ CPP_fast_reviewing/ch05_8b.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+char c = 'a';
+char& r() {
+	return c;
+}
+char* p() {
+	return &c;
+}
+
+int main() {
+	cout << "c 값 : " << c << endl;
+	
+	r() = 'C';
+	cout << "r() = 'C' 수행 후 c 값 : " << c << ", r() 값 : " << r() << endl;
+
+	char* s = p();
+	cout << "char* s = p() 수행 후 c 값 : " << c << ", r() 값 : " << r() << ", s 값 : " << *s << endl;
+
+	*s = 'B';
+	cout << "*s = 'B' 수행 후 c 값 : " << c << ", r() 값 : " << r() << ", s 값 : " << *s << endl;
+}

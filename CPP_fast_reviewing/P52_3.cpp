56a5fdfb857b951f25b43cdac65ff3ab5f1ae9f9
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-10-13 07:02:20 +0900

    CH01_Practice

---
+++ CPP_fast_reviewing/P52_3.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int n = 15;
+	int m = 4;
+	int sum = n % m;
+
+	cout << n << "를" << m << "로 나눈 나머지는 " << sum << "입니다.";
+}
---
+++ CPP_fast_reviewing/P52_4.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "*\n";
+	cout << "**\n";
+	cout << "***\n";
+	cout << "****\n";
+}

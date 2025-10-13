d397157c3ecdbd870b5646abb35806de543b61a8
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-10-13 11:20:38 +0900

    디폴트 매개 변수를 가진 함수 선언 및 호출 ver ori

---
+++ CPP_fast_reviewing/ch06_3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+void star(int a = 5);
+void msg(int id, string text = "world");
+
+void star(int a) {
+	for (int i = 0; i < a; i++) {
+		cout << "*";
+	}
+	cout << endl;
+}
+
+void msg(int id, string text) {
+	cout << id << ' ' << text << endl;
+}
+
+int main() {
+	star();
+	star(10);
+
+	msg(10);
+	msg(10, "Hello");
+}

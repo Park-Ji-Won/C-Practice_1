67917b291d238074db8030ef825056d502324a36
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-10-13 11:46:02 +0900

    전역 함수 사용 나쁜 사례

---
+++ CPP_fast_reviewing/ch06_9a.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int abs(int a) {
+	return (a > 0) ? a : -a;
+}
+int man(int a, int b) {
+	return (a > b) ? a : b;
+}
+int min(int a, int b) {
+	return (a > b) ? b : a;
+}
+
+int main() {
+	cout << "절대값 : " << abs(-5) << endl;
+	cout << "최대값 : " << max(10, 8) << endl;
+	cout << "최소값 : " << min(-3, -8) << endl;
+}

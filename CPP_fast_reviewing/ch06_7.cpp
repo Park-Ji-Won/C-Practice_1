877460255fa01c2e822cb897097b9950ed77a210
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-10-13 11:27:04 +0900

    형 변환으로 인해 함수 중복이 모호한 경우

---
+++ CPP_fast_reviewing/ch06_7.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+float square(float a) {
+	cout << "float : " << endl;
+	return a * a;	
+}
+
+double square(double a) {
+	cout << "double : " << endl;
+	return a * a;
+}
+
+int main() {
+	cout << square(3, 0);
+	cout << square(3);
+}

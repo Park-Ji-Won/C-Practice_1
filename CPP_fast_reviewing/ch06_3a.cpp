5a5caf3f414c1ddd7fa5ce58c13cf4ed0bdecee9
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-10-13 11:20:51 +0900

    디폴트 매개 변수를 가진 함수 선언 및 호출 ver 2

---
+++ CPP_fast_reviewing/ch06_3a.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+void star(int a = 5) {
+	for (int i = 0; i < a; i++) {
+		cout << '*';
+	}
+	cout << endl;
+}
+
+void msg(int id, string text = "") {
+	cout << id << ' ' << text << endl;
+}
+
+
+int main() {
+	star(5);
+	star(10);
+
+	msg(10,"");
+	msg(10, "Hello");
+}

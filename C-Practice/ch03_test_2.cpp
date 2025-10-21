119323ae6e8b466cc2406f170bd5bc3d5dae7fa9
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-10-21 14:39:51 +0900

    생성자 생성 및 소멸 순서

---
+++ C-Practice/ch03_test_2.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+
+class Circle {
+public:
+	int radius;
+	Circle();
+	Circle(int r);
+	~Circle();
+	double getArea();
+};
+
+Circle::Circle() {
+	radius = 1;
+	cout << "반지름 " << radius << " 원 생성" << endl;
+}
+
+Circle::Circle(int r) {
+	radius = r;
+	cout << "반지름 " << radius << " 원 생성" << endl;
+}
+
+Circle::~Circle() {
+	cout << "반지름 " << radius << " 원 소멸" << endl;
+}
+
+double Circle::getArea() {
+	return 3.14 * radius * radius;
+}
+
+// 1 전역 객체 생성
+Circle globalDount(1000);
+Circle globalPizza(2000);
+
+void f() {
+	// 2 지역 객체 생성
+	Circle fDonut(100);
+	Circle fPizza(200);
+}
+
+int main() {
+	// 3 지역 객체 생성
+	Circle mainDount;
+	Circle mainPizza(30);
+	f();
+
+	// 4 지역 객체 생성
+	Circle mainDount1;
+	Circle mainPizza1(300);
+}

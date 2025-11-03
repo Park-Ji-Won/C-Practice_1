b8a01dead96dbdc88e2a3197c10ff2da6827de73
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-11-03 23:00:32 +0900

    두 Power 객체를 더하는 += 연산자(참조 리턴 방식) 작성(교재 변형 문제)

---
+++ CPP_fast_reviewing/ch07_6.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+class Power {
+	int kick;
+	int punch;
+public:
+	Power(int kick = 0, int punch = 0) {
+		this->kick = kick;
+		this->punch = punch;
+	}
+	void show();
+	Power operator+(Power op2);
+	Power& operator+= (Power op2);
+};
+
+void Power::show() {
+	cout << "kick=" << kick << ',' << "punch=" << punch << endl;
+}
+
+Power Power::operator+(Power op2) {
+	Power tmp;
+	tmp.kick = this->kick + op2.kick;
+	tmp.punch = this->punch + op2.punch;
+	return tmp;
+}
+
+Power& Power::operator+=(Power op2) {
+	kick = kick + op2.kick;
+	punch = punch + op2.punch;
+	return *this;
+}
+
+
+int main() {
+	Power a(3, 5), b(4, 6), c, d;
+	a.show();
+	b.show();
+	c = a + b;
+	c.show();
+	d = a += b;
+	a.show();
+	d.show();
+}

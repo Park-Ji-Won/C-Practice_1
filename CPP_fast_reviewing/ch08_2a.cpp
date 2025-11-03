b65172731554e664371bbefa5bf96482287f34dd
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-11-04 00:14:48 +0900

    파생 클래스의 호출

---
+++ CPP_fast_reviewing/ch08_2a.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class A {
+public:
+	A() {
+		cout << "생성자 A call" << endl;
+	}
+	~A() {
+		cout << "생성자 A recall" << endl;
+	}
+};
+
+class B : public A {
+public:
+	B() {
+		cout << "생성자 B call" << endl;
+	}
+	~B() {
+		cout << "생성자 B recall" << endl;
+	}
+};
+
+class C : public B {
+public:
+	C() {
+		cout << "생성자 C call" << endl;
+	}
+	~C() {
+		cout << "생성자 C recall" << endl;
+	}
+};
+
+int main() {
+	C c;
+}

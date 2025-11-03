a5f581547210e03fe33c6416eeb69cc688952c01
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-11-03 22:42:27 +0900

    클래스의 멤버 함수로 선언
    Color operator + (Color & op2);   // 멤버 함수 선언을 참조하여 구현
    bool  operator == (Color &op2);

---
+++ CPP_fast_reviewing/ch07_op2_2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+class Color {
+private:
+	int red, green, blue;
+public:
+	Color(int r = 0, int g = 0, int b = 0) :red(r), green(g), blue(b) {}
+	Color operator+ (Color& op2) {
+		return Color(red + op2.red,
+			green + op2.green,
+			blue + op2.blue);
+	}
+	bool operator ==(Color op2) {
+		return(red == op2.red &&
+			green == op2.green &&
+			blue == op2.blue);
+	}
+	void show() {
+		cout << "(" << red << ", " << green << ", " << blue << ")" << endl;
+	}
+};
+int main() {
+	Color c1(100, 150, 200);
+	Color c2(50, 100, 55);
+	Color c3 = c1 + c2;
+	Color c4(150, 250, 255);
+	cout << "c1 + c2 = ";
+	c3.show();
+	if (c3 == c4)
+		cout << "c3 and c4 are equal" << endl;
+	else
+		cout << "c3 and c4 are NOT equal" << endl;
+}

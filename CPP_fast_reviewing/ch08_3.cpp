7a82b3470394118acf20202e750aeb68827f32b7
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-11-04 00:21:33 +0900

    end 11/04 AM12:21

---
+++ CPP_fast_reviewing/ch08_3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class TV {
+	int size;
+public:
+	TV() {
+		size = 20;
+	}
+	TV(int size) {
+		this->size = size;
+	}
+	int getSize() {
+		return size;
+	}
+};
+

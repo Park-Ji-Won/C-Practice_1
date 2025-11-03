0773e27921293843dceb5adb4703c065bb1545ab
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-11-03 23:59:39 +0900

    다운 캐스팅 문제점 / 수정

---
+++ CPP_fast_reviewing/ch08_casting_issue_solved.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Point {
+protected:
+	int x, y;
+public:
+	Point(int a = 0, int b = 0) : x(a), y(b) {}
+	void show() {
+		cout << "Point : " << x << ", " << y << endl;
+	}
+};
+
+class ColorPoint : public Point {
+	string color;
+public:
+	ColorPoint(int a = 0, int b = 0, string c = "BLACK") :
+		Point(a, b), color(c) {
+	}
+	void setColor(string c) {
+		color = c;
+	}
+	void show() {
+		cout << color << "Point : " << x << ", " << y << endl;
+	}
+};
+
+int main() {
+	Point p(1, 2);
+	//Point* pBase = &p;
+	ColorPoint cp(3, 4);
+	Point* pBase = &cp; // 업 캐스팅
+	// 잘못된 다운 캐스팅
+	ColorPoint* pDer = (ColorPoint*)pBase;
+	cout << "다운 캐스팅 강제 수행 후" << endl;
+	pDer->setColor("YELLOW");
+	pDer->show();
+}

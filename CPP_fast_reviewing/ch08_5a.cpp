de578c37cf540a0b5c69d3f9ebd0ad17f63bd66e
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-11-04 20:57:21 +0900

    public 상속 문제

---
+++ CPP_fast_reviewing/ch08_5a.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+class Base {
+	int a;
+protected:
+	void setA(int a) {
+		this->a = a;
+	}
+public:
+	void showA() {
+		cout << a;
+	}
+};
+
+class Derived : public Base {
+	int b;
+protected:
+	void setB(int b) {
+		this->b = b;
+	}
+public:
+	void showB() {
+		cout << b;
+	}
+};
+
+int main() {
+	Derived x;
+	x.a = 5; // 1 Base 클래스의 private 멤버로 접근 불가 
+	x.setA(10);  // 2 Base 클래스의 protected이고, public으로 상속되어 protected 유지됨.따라서 Derived 클래스 외부(main)에서 접근 불가
+	x.showA();  // 3 
+	x.b = 10;  // 4 x.b에서 b가 private이므로 외부 함수 main 접근 불가
+	x.setB(10);  // 5 x.setB(10) protected이므로 외부 함수 main 접근 불가
+	x.showB();  // 6
+}

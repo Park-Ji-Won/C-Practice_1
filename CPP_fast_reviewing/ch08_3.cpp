8a43fb82c9b558d0269950b221d5317084dfe193
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-11-04 20:45:01 +0900

    TV, WideTV, SmartTV 생성자 매개 변수 전달

--- CPP_fast_reviewing/ch08_3.cpp
+++ CPP_fast_reviewing/ch08_3.cpp
@@ -16,3 +16,32 @@ public:
 	}
 };
 
+class WideTV : public TV {
+	bool videoln;
+public:
+	WideTV(int size, bool videoln) :TV(size) {
+		this->videoln = videoln;
+	}
+	bool getVideoln() {
+		return videoln;
+	}
+};
+
+class SmartTV : public WideTV {
+	string ipAddr;
+public:
+	SmartTV(string ipAddr, int size) : WideTV(size, true) {
+		this->ipAddr = ipAddr;
+	}
+	string getIpAddr() {
+		return ipAddr;
+	}
+};
+
+int main() {
+	SmartTV htv("192.0.0.1", 32);
+	cout << "size=" << htv.getSize() << endl;
+	cout << "videoln=" << boolalpha << htv.getVideoln() << endl;
+	cout << "IP=" << htv.getIpAddr() << endl;
+}
+

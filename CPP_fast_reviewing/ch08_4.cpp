0d904c4c0a277f7c4d021a1faf28478f1d47a2a9
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-11-04 20:51:45 +0900

    private 상속 사례

---
+++ CPP_fast_reviewing/ch08_4.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+class Base {
+	int a;
+protected:
+	void setA(int a) {
+		this->a = a;
+	}
+public:
+	void showA() {
+		cout << a;
+	}
+};
+
+class Derived : private Base {
+	int b;
+protected:
+	void setB(int b) {
+		this->b = b;
+	}
+public:
+	void showB() {
+		cout << b;
+	}
+};
+
+int main() {
+	Derived x;
+	x.a = 5; // 1 Base 클래스의 private 멤버로 접근 불가 
+	x.setA(10); // 2
+ 	x.showA(); // 3 Base 클래스의 protected, public 이지만, private 상속되어 Derived 클래스에서 접근 불가
+	x.b = 10; // 4 x.b에서 b가 private이므로 외부 함수 main 접근 불가
+	x.setB(10); //5 x.setB(10) protected이므로 외부 함수 main 접근 불가
+	x.showB(); //6
+}

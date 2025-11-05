f859590a0393583637f4754f8e28012c4f75a88b
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-11-05 21:06:32 +0900

    Ch08-5a public 상속 문제 해결 방안

---
+++ CPP_fast_reviewing/ch08_5a_modi.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+class Base {
+	int a;
+protected:
+	void setA(int a) {
+		this->a = a;
+	}
+public:
+	void inputA(int a) {
+		setA(a);
+	}
+	void showA() {
+		cout << "a : " << a << endl;
+	}
+};
+
+class Derived : public Base {
+	int b;
+protected:
+	void setB(int b) {
+		this->b = b;
+	}
+public:
+	void inputB(int b) {
+		setB(b);
+	}
+	void showB() {
+		cout << "b : " << b << endl;
+	}
+};
+
+int main() {
+	Derived x;
+	x.inputA(10);
+	x.inputA(20);
+	x.showA();
+	x.inputB(30);
+	x.inputB(40);
+	x.showB();
+}

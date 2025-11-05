3d5b2d96fcdbc22238e7be5f9b373dd11c909098
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-11-05 21:26:07 +0900

    다중 상속 선언 및 멤버 호출

---
+++ CPP_fast_reviewing/ch08_7a.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+using namespace std;
+
+class MP3 {
+public:
+	void play();
+	void stop();
+};
+
+void MP3::play() {
+	cout << "MP3 play()" << endl;
+}
+
+void MP3::stop() {
+	cout << "MP3 stop()" << endl;
+}
+
+class MobilePhone {
+public:
+	bool sendCall();
+	bool receiveCall();
+	bool sendSMS();
+	bool receiveSMS();
+};
+
+bool MobilePhone::sendCall() {
+	return true;
+}
+bool MobilePhone::receiveCall() {
+	return true;
+}
+bool MobilePhone::sendSMS() {
+	return false;
+}
+bool MobilePhone::receiveSMS() {
+	return false;
+}
+
+class MusicPhone : public MP3, public MobilePhone {
+public:
+	void dial();
+};
+
+void MusicPhone::dial() {
+	play();
+	sendCall();
+	cout << "MusicPhone::dial()" << endl;
+}
+
+int main() {
+	MusicPhone hanPhone;
+	hanPhone.play();
+	cout << "hanPhone.sned : " << hanPhone.sendSMS();
+}

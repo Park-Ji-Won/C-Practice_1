17bb6744115a91e11ed623086f49fffc75119b00
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-11-05 21:37:50 +0900

    동적 바인딩 실습

---
+++ CPP_fast_reviewing/ch08_binding.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+	virtual void speak() {
+		cout << "Animal speaks" << endl;
+	}
+};
+
+class Dog : public Animal {
+public:
+	void speak() override {
+		cout << "Dog barks" << endl;
+	}
+};
+
+class Cat : public Animal {
+public:
+	void sepak() {
+		cout << "Cat meows" << endl;
+	}
+};
+
+void makesound(Animal* a) {
+	a->speak();
+}
+
+int main() {
+	Animal a;
+	Dog d;
+	Cat c;
+	makesound(&a);
+	makesound(&d);
+	makesound(&c);
+}

49c231315f86d911addb5979e389014f91f2e876
Park-Ji-Won <dpfmwptm01@gmail.com>
2025-11-20 04:49:29 +0900

    템플릿 실습

---
+++ CPP_fast_reviewing/ch10_T.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+template <typename T>
+void myswap(T& a, T& b) {
+	T tmp;
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+int main() {
+	int a = 3, b = 5;
+	myswap(a, b);
+	cout << "a = " << a << ", b = " << b << endl;
+	
+	double c = 0.3, d = 12.5;
+	myswap(c, d);
+	cout << "c = " << c << ", d = " << d << endl;
+
+	char e = 'e', f = 'f';
+	myswap(e, f);
+	cout << "e = " << e << ", f = " << f << endl;
+}
